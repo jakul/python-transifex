60be317f765832b36db61653f7311b9582311912
Craig Blaszczyk <masterjakul@gmail.com>
2012-01-06 12:36:24 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-transifex',
+    version='0.0.1-dev',
+    description='A python api to transifex',
+    long_description=open('README.rst').read(),
+    author='Craig Blaszczyk',
+    author_email='masterjakul@gmail.com',
+    url='https://github.com/jakul/python-transifex',
+    download_url='https://github.com/jakul/python-transifex/downloads',
+    license='BSD',
+    packages=find_packages(),
+    tests_require=[
+        'django>=1.2,<1.4',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
