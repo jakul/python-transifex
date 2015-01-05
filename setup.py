57705db8da89987fded35ab9da154631c5ebc36b
Craig Blaszczyk <masterjakul@gmail.com>
2015-01-05 18:14:19 +0000

    rename README.rst to README.md; add command to upload to pypi and tag

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
+import os
 from setuptools import setup, find_packages
+import sys
 
 from transifex import VERSION
+
+if sys.argv[-1] == 'publish-to-pypi':
+    os.system("python setup.py sdist upload -r pypi")
+    os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
 
 setup(
     name='python-transifex',
     version=VERSION,
     description='A python api to transifex',
-    long_description=open('README.rst').read(),
     author='Craig Blaszczyk',
     author_email='masterjakul@gmail.com',
     url='https://github.com/jakul/python-transifex',
-    download_url='https://github.com/jakul/python-transifex/downloads',
     license='BSD',
     packages=find_packages(),
     tests_require=['mock',
